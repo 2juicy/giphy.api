14c93d33dddf8f8d0796e7783285f2748867f902
JohnnyC <j0hnnywchiang@gmail.com>
2018-05-18 23:12:33 -0700

    clearing div fix with a better display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //Global Variables
-var topics = ['Overwatch', 'Hearthstone'];
+var topics = ['Overwatch', 'Hearthstone', 'Diablo 3'];
 //function for AJAX to populate page
 function displayInfo(){
     var search = $(this).attr("data-name");
@@ -9,22 +9,44 @@ function displayInfo(){
     $.ajax({
           url: queryURL,
           method: "GET"
-        }).then(function(response) {
-        console.log(response);
-        //   $("#gif").text(JSON.stringify(response));       
-        //stores image in a var.
-        var imageUrl = response.data;
-        var p = $('<p>').text('Rating: ' + rating);
-        //setup to create <img> element
-        var gameImage = $("<div class='g'>");
-        //sets source of image
-        gameImage.attr("src", imageUrl);
-        //sets an alternative if image does not come on screen
-        gameImage.attr("alt", "game image");
-        //posts the image in a new <div>
-        $("#images").prepend(gameImage);
+        }).then(function(response) { 
+        var result = response.data;
+        //added empty to fix spam.
+        $('gif').empty();
+        //loop to make each image
+        for (let i = 0; i < result.length; i++){
+            //declaring our variable to create new Div
+            var newDiv = $("<div class='g'>");
+            //rating
+            var rating = result[i].rating;
+            var p = $('<p>').text('Rating: ' + rating);
+            //image location with animate and still attributes added.
+            console.log(result[i].images);
+            var sImage = $('<img>').attr('src', result[i].images.fixed_height_still.url);
+            sImage.addClass('gif');
+            sImage.attr('data-still', result[i].images.fixed_height_still.url);
+            sImage.attr('data-animate', result[i].images.fixed_height.url);
+            sImage.attr('data-state', 'still');
+            //attaching both elemnts image and rating to div
+            newDiv.append(p);
+            newDiv.append(sImage);
+            //attaching newDiv to HTML page showing AJAX call items.
+            $('#gif').prepend(newDiv);
+        };
+        //Function to change state
+        $('.gif').on('click', function(){
+            var state = $(this).attr('data-state');
+            if (state == 'still') {
+                $(this).attr('src', $(this).attr('data-animate'));
+                $(this).attr('data-state', 'animate');
+            } else {
+                $(this).attr('src', $(this).attr('data-still'));
+                $(this).attr('data-state', 'still');
+            }
+        });
     });
 }
+
 //Function for appending buttons
 function renderButtons() {
     $("#buttons").empty();
@@ -36,7 +58,6 @@ function renderButtons() {
         $("#buttons").append(a);
     }
 }
-
 //on.click to add buttons
 $("#addGame").on("click", function(event) {
     event.preventDefault();
