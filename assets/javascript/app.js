cbc53daa1f5846e69a91593f678fe0f51e19df6e
JohnnyC <j0hnnywchiang@gmail.com>
2018-05-19 00:58:26 -0700

    tried bonus and style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,45 @@
 //Global Variables
-var topics = ['Overwatch', 'Hearthstone', 'Diablo 3'];
+var topics = ['Overwatch', 'Hearthstone', 'Starcraft 2'];
 //function for AJAX to populate page
 function displayInfo(){
     var search = $(this).attr("data-name");
-    console.log(search);
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dc6zaTOxFJmzC&limit=10";
-    console.log(queryURL);
     $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(response) { 
         var result = response.data;
-        console.log(result);
         //added empty to fix spam.
-        $('gif').empty();
+        $('#gif').empty();
         //loop to make each image
         for (let i = 0; i < result.length; i++){
             //declaring our variable to create new Div
             var newDiv = $("<div class='g'>");
             //rating
             var rating = result[i].rating;
-            var p = $('<p>').text('Rating: ' + rating);
+            var p = $("<p class='rate'>").text('Rating: ' + rating);
+            var t = result[i].title;
+            var title = $("<p class='title'>").text(t);
             //image location with animate and still attributes added.
-            console.log(result[i].images);
+            console.log(result[i]);
             var sImage = $('<img>').attr('src', result[i].images.fixed_height_still.url);
             sImage.addClass('gif');
             sImage.attr('data-still', result[i].images.fixed_height_still.url);
             sImage.attr('data-animate', result[i].images.fixed_height.url);
             sImage.attr('data-state', 'still');
-            //attaching both elemnts image and rating to div
-            newDiv.append(p);
+            //creating download link
+            var dLink = result[i].images.original.url;  
+            //Not sure why 1 click download doesn't work tried.
+            var addLink = $("<a href=" + dLink + " download target='_blank'>")
+            var d = $("<p class='download' download>").text('Download Original');
+            addLink.append(d);
+            //attaching both elements, image, link and rating to div
+            newDiv.append(title);            
+            newDiv.append(p);    
             newDiv.append(sImage);
+            newDiv.append(addLink);
             //attaching newDiv to HTML page showing AJAX call items.
-            $('#gif').prepend(newDiv);
+            $('#gif').prepend(newDiv);            
         };
         //Function to change state on click
         $('.gif').on('click', function(){
